9ed3723b90a1abbc3f8f4ff62919c2bc3f603da3
nathyong <nathyong@gmail.com>
2015-12-14 08:32:19 +1100

    Do not open index file unneccessarily

--- parse.py
+++ parse.py
@@ -62,10 +62,9 @@ def main():
     index_filename = os.path.abspath(args.archive_file + '.index')
 
     if not os.path.isfile(index_filename):
-        with open(index_filename, 'w') as index_file:
-            tar_args = ['tar', '--index-file', index_filename, '-tvf',
-                        args.archive_file]
-            subprocess.call(tar_args)
+        tar_args = ['tar', '--index-file', index_filename, '-tvf',
+                    args.archive_file]
+        subprocess.call(tar_args)
 
     with open(index_filename) as index_file:
         tree = read_index_file(index_file)
